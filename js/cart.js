6d69c607cd6b3e7c55d67aa93e8e95e9e6e67d62
范优 <876255983@qq.com>
2019-07-17 20:33:42 +0800

    总价和总和，没有完成

--- js/cart.js
+++ js/cart.js
@@ -1,17 +1,17 @@
 $(() => {
-    // 跳转之后，要在页面上读取数据
-    let jsonStr = localStorage.getItem('shopCartData');
-    // console.log(jsonStr);  输出的是一个
-    // 判断jsonStr是否为null如果是null就没有数据，如果不是null，就是有数据，需要生成购物车的商品列表
-    let arr = [];
-    // 如果不是null，就是有数据，需要生成购物车的商品列表            如果是null 那就是没有数据， 没有数据 就不用else ，会弹出空空如也
-    if (jsonStr !== null) {
-        // 把读取出来的数据放进来 循环找
-        arr = JSON.parse(jsonStr);
-        let html = '';
-        // 遍历
-        arr.forEach(e => {
-            html += `<div class="item" data-id="${e.pID}">
+  // 跳转之后，要在页面上读取数据
+  let jsonStr = localStorage.getItem('shopCartData');
+  // console.log(jsonStr);  输出的是一个
+  // 判断jsonStr是否为null如果是null就没有数据，如果不是null，就是有数据，需要生成购物车的商品列表
+  let arr;
+  // 如果不是null，就是有数据，需要生成购物车的商品列表            如果是null 那就是没有数据， 没有数据 就不用else ，会弹出空空如也
+  if (jsonStr !== null) {
+    // 把读取出来的数据放进来 循环找
+    arr = JSON.parse(jsonStr);
+    let html = '';
+    // 遍历
+    arr.forEach(e => {
+      html += `<div class="item" data-id="${e.pID}">
             <div class="row">
               <div class="cell col-1 row">
                 <div class="cell col-1">
@@ -44,12 +44,19 @@ $(() => {
               </div>
             </div>
           </div>`
-        });
-        $('.item-list').html(html);
-        // 因为已经生成了结构，所以把空空如也隐藏掉
-        $('.empty-tip').hide();
-        // 把表头+总计显示出来
-        $('.cart-header').removeClass('hidden');
-        $('.total-of').removeClass('hidden');
-    }
+    });
+    $('.item-list').html(html);
+    // 因为已经生成了结构，所以把空空如也隐藏掉
+    $('.empty-tip').hide();
+    // 把表头+总计显示出来
+    $('.cart-header').removeClass('hidden');
+    $('.total-of').removeClass('hidden');
+  }
+
+  // 计算总和和总价
+  function computedCountAndMoney() {
+    // 算出总计里面的总数量和总价
+    // 根据选中的多选框，得到选中的商品的id
+
+  }
 });
